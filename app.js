3e367c3cbd13e230ef3c33e864a202e199ed5d5e
ThomassMathisen <thomasmathisen@hotmail.com>
2022-10-08 15:10:55 +0200

    Add .env file for security

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -12,7 +15,7 @@ var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://thomat:thomat123@cluster0.lflruz1.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lflruz1.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
